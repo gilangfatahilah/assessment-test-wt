b88f86fcffde41f99851376870df7f797c400a35
Gilang Fatahilah <gilangf010@gmail.com>
2024-10-15 00:48:25 +0700

    🔧 build(config): vite build config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 
 export default {
   root: path.resolve(__dirname, 'src'),
@@ -10,5 +10,28 @@ export default {
   server: {
     port: 8080,
     hot: true
+  },
+  build: {
+    target: 'es2015',
+    outDir: path.resolve(__dirname, 'dist'),
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    cssCodeSplit: true,
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
   }
-}
+};
